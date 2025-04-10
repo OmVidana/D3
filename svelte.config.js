23bb01147dfadd982cd832a008d0c3d3508ed704
Omar Vidaña Rodríguez <omviro@gmail.com>
2025-04-10 09:03:25 -0600

    GP - Fix 16

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,11 @@
 // import adapter from '@sveltejs/adapter-cloudflare';
-import { base } from '$app/paths'
 import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			fallback: `${base}/404.html`
-		}),
+		adapter: adapter(),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
